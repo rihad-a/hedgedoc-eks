cfb519f2c5f7139bcb105c51cd6b219ad5a52bd4
rihad-a <Rihadwork@hotmail.com>
2026-02-04 21:28:28 +0000

    Attaching AmazonElasticFileSystemFullAccess policy to eks node group

--- terraform/deployment/modules/eks/main.tf
+++ terraform/deployment/modules/eks/main.tf
@@ -96,6 +96,7 @@ resource "aws_eks_node_group" "eks-node-group" {
     aws_iam_role_policy_attachment.AmazonEKSWorkerNodePolicy,
     aws_iam_role_policy_attachment.AmazonEKS_CNI_Policy,
     aws_iam_role_policy_attachment.AmazonEC2ContainerRegistryReadOnly,
+    aws_iam_role_policy_attachment.AmazonElasticFileSystemFullAccess,
   ]
 }
 
@@ -126,6 +127,11 @@ resource "aws_iam_role_policy_attachment" "AmazonEKS_CNI_Policy" {
 
 resource "aws_iam_role_policy_attachment" "AmazonEC2ContainerRegistryReadOnly" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+  role       = aws_iam_role.eks-ng-role.name
+}
+
+resource "aws_iam_role_policy_attachment" "AmazonElasticFileSystemFullAccess" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonElasticFileSystemFullAccess"
   role       = aws_iam_role.eks-ng-role.name
 }
 
